f78d3d36cdd78d60d329412adc1e3bb158c13a19
JoseEscobar128 <guillermoescobar128@gmail.com>
2023-12-17 16:40:14 -0600

    ya funciona el registro correctamente

---
+++ Integradora/Controller/Login/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Integradora
+//
+//  Created by Jose Escobar on 17/12/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var txtContrasena: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnIngresar(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Integradora/Controller/Login/RegisterViewController.swift
@@ -0,0 +1,97 @@
+//
+//  RegisterViewController.swift
+//  Integradora
+//
+//  Created by Jose Escobar on 17/12/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var txtContrasena: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtNombre: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnRegistro(_ sender: Any) {
+        guard let url = URL(string: "http://3.129.244.114/api/register") else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let usuarioData = ["name": txtNombre.text,
+                           "email": txtEmail.text,
+                           "password": txtContrasena.text]
+
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: usuarioData, options: [])
+            request.httpBody = jsonData
+
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    self.mostrarAlerta(mensaje: "Error al realizar la solicitud: \(error.localizedDescription)")
+                    return
+                }
+
+                if let httpResponse = response as? HTTPURLResponse, let data = data {
+                    do {
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+
+                        switch httpResponse.statusCode {
+                        case 200:
+                            if let msg = jsonResponse?["msg"] as? String, msg.lowercased().contains("ya existe") {
+                                self.mostrarAlerta(mensaje: msg)
+                            } else {
+                                self.mostrarAlerta(mensaje: "Error en la respuesta del servidor (200).")
+                            }
+                        case 201:
+                            if let msg = jsonResponse?["msg"] as? String {
+                                self.mostrarAlerta(mensaje: msg)
+                            } else {
+                                self.mostrarAlerta(mensaje: "Error en la respuesta del servidor.")
+                            }
+                        case 422:
+                            if let errorMsg = jsonResponse?["data"] as? [String: Any] {
+                                var errorMessage = ""
+                                for (key, value) in errorMsg {
+                                    if let errors = value as? [String] {
+                                        errorMessage += "\(key.capitalized): \(errors.joined(separator: ", "))\n"
+                                    }
+                                }
+                                self.mostrarAlerta(mensaje: errorMessage)
+                            } else {
+                                self.mostrarAlerta(mensaje: "Error en la respuesta del servidor (422).")
+                            }
+                        default:
+                            self.mostrarAlerta(mensaje: "Error en la respuesta del servidor. Código de estado: \(httpResponse.statusCode)")
+                        }
+                    } catch {
+                        self.mostrarAlerta(mensaje: "Error al analizar la respuesta JSON.")
+                    }
+                }
+            }
+
+            task.resume()
+
+        } catch {
+            self.mostrarAlerta(mensaje: "Error al convertir datos a JSON: \(error.localizedDescription)")
+        }
+    }
+
+    // Función para mostrar una alerta en la vista
+    func mostrarAlerta(mensaje: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Mensaje", message: mensaje, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
